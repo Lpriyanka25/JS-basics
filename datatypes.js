05130f89d34a4ff12e877c529573decd2a63421e
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-02-17 23:26:02 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newr version
+
+// alert(3 + 3) // we are using node JS not browser so it gives error
+
+console.log(3 + 3);
+
+let age = 18;
+let isLoggedIn = false
+let State;
+
+// number = 2 to power of 53
+// bigint
+// string = "" 
+// boolean = true / false
+// null = standalone value 
+// undefined 
+// symbol = unique
+
+
+// object 
+
+console.log(typeof "Priyanka"); // string
+
+console.log(typeof null); // null is object
+
+console.log(typeof undefined); // undefined is undefined
